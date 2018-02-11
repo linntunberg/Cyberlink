ca524b30a5c1cc626befa5946be5a7e1df3046d5
linntunberg <31646491+linntunberg@users.noreply.github.com>
2018-02-12 00:06:08 +0100

    Update functions.php

--- app/functions.php
+++ app/functions.php
@@ -3,13 +3,7 @@
 declare(strict_types=1);
 
 if (!function_exists('redirect')) {
-    /**
-     * Redirect the user to given path.
-     *
-     * @param string $path
-     *
-     * @return void
-     */
+ 
     function redirect(string $path)
     {
         header("Location: ${path}");
