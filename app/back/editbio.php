ef8f269fd8ea8674fe853d319d475024e57af0a1
linntunberg <linn.tunberg@gmail.com>
2018-02-19 15:19:42 +0100

    Added

--- app/back/editbio.php
+++ app/back/editbio.php
@@ -1,2 +1,13 @@
 <?php declare(strict_types=1);
 require __DIR__.'/../autoload.php';
+
+if(isset($_POST['biography'])) {
+  $email = filter_var($_POST['biography']);
+
+  $statement = $pdo->prepare('UPDATE users SET biography =:biography WHERE userID = :id');
+  $statement->bindParam(':id', $_SESSION['user']);
+  $statement->bindParam(':biography', $biography);
+  $statement->execute();
+
+redirect('../../profile.php');
+}
