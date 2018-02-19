85ab02b0b38ea2712e44f0d2bea42c7745e302e1
linntunberg <linn.tunberg@gmail.com>
2018-02-19 15:32:42 +0100

    Changes

--- editprofile.php
+++ editprofile.php
@@ -10,14 +10,14 @@ $user = $statement->fetch(PDO::FETCH_ASSOC);
 
 <h1>Edit Account</h1>
 
-<form method="POST" action="/app/back/editEmail.php"><br>  <!-- sends the input to the file  (back) editprofile.php  -->
+<form method="POST" action="/app/back/editprofile.php"><br>  <!-- sends the input to the file  (back) editprofile.php  -->
     <div class="form">
 <strong>Change email: </strong> <input type="text" name="email" value="<?php echo $user['email']?>" required><br>
 <br><button type="submit">Confirm</button>
 </div>
 </form>
 
-<form method="POST" action="/app/back/editpassword.php"><br>  <!-- sends the input to the file  (back) editprofile.php  -->
+<form method="POST" action="/app/back/editprofile.php"><br>  <!-- sends the input to the file  (back) editprofile.php  -->
     <div class="form">
 <strong>Old password: </strong> <input type="password" name="password" required><br>
 <strong>New password: </strong> <input type="password" name="password" required><br>
